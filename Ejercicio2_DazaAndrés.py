a277c6c3cf0aa1d02e109f4b945aa375af05a706
andresDZ2024 <daza45daza@gmail.com>
2024-01-26 13:21:44 -0500

    Inicio tarea2 python

---
+++ Ejercicio2_DazaAndrés.py
@@ -0,0 +1,14 @@
+## -------------------------------------------------
+## ------------------ Ejercicio2 -------------------
+## -------------------------------------------------
+## ------------------- arreglos --------------------
+
+A = 0
+B = 1
+
+print("       Bienvenido usuario, te voy a explicar como funciona la serie de fibonacci: ")
+print("-----------------------------------------------------------------------------------")
+print("""       Se trata de una secuencia infinita de números naturales;
+       a partir del 0 y el 1, se van sumando a pares, de manera que
+       cada número es igual a la suma de sus dos anteriores""")
+N = input(print(str("ingresa hasta que numero de la secuencia fibonacci deseas ver")))
